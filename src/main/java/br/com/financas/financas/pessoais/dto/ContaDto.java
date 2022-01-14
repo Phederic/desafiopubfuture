eb5074583257afbf247277fd06d081326650e929
Phederic <87782988+Phederic@users.noreply.github.com>
2022-01-14 17:58:39 -0300

    Tentando criar o método de transferir

--- src/main/java/br/com/financas/financas/pessoais/dto/ContaDto.java
+++ src/main/java/br/com/financas/financas/pessoais/dto/ContaDto.java
@@ -2,10 +2,12 @@ package br.com.financas.financas.pessoais.dto;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
 import br.com.financas.financas.pessoais.modelo.Conta;
 import br.com.financas.financas.pessoais.modelo.TipoConta;
+import br.com.financas.financas.pessoais.repository.ContaRepository;
 
 public class ContaDto {
 
@@ -13,6 +15,11 @@ public class ContaDto {
 	private BigDecimal saldo;
 	private String instituicaoFinanceira;
 	private TipoConta tipoConta;
+	
+	@Autowired
+	ContaRepository contaRepository;
+	
+	
 
 	public ContaDto(Conta conta) {
 		this.contaId = conta.getContaId();
@@ -36,9 +43,27 @@ public class ContaDto {
 	public TipoConta getTipoConta() {
 		return tipoConta;
 	}
+	
+	public void setContaId(Integer contaId) {
+		this.contaId = contaId;
+	}
+
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	public void setInstituicaoFinanceira(String instituicaoFinanceira) {
+		this.instituicaoFinanceira = instituicaoFinanceira;
+	}
+
+	public void setTipoConta(TipoConta tipoConta) {
+		this.tipoConta = tipoConta;
+	}
 
 	public static Page<ContaDto> converter(Page<Conta> conta) {
 		return conta.map(ContaDto::new);
 	}
 
+	
+ 
 }
