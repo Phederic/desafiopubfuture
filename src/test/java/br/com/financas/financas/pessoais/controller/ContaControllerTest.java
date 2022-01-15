11ad3f2d39ea55cfe90fdfcebf8d11fbb555f40f
Phederic <87782988+Phederic@users.noreply.github.com>
2022-01-15 13:25:27 -0300

    TestContaController

---
+++ src/test/java/br/com/financas/financas/pessoais/controller/ContaControllerTest.java
@@ -0,0 +1,81 @@
+package br.com.financas.financas.pessoais.controller;
+
+
+import java.net.URI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class ContaControllerTest {
+
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testCadastrarComDados() throws Exception {
+		URI uri = new URI("/conta");
+		String json = "{}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(201));
+	}
+
+	@Test
+	public void testListar() throws Exception {
+		URI uri = new URI("/conta");
+		String json = "{}";
+
+		mockMvc.perform(MockMvcRequestBuilders.get(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+
+	@Test
+	public void testDeletar() throws Exception {
+		URI uri = new URI("/conta/5");
+		String json = "{\"/{id}\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.delete(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+
+	@Test
+	public void testPesquisarPorID() throws Exception {
+		URI uri = new URI("/conta/10");
+		String json = "{\"/{contaId}\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.get(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+
+	@Test
+	public void testModificar() throws Exception {
+		URI uri = new URI("/conta/2");
+		String json = "{\"saldo\": 1800}";
+
+		mockMvc.perform(MockMvcRequestBuilders.put(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+	
+	
+	@Test
+	public void testSaldoTotal() throws Exception {
+		URI uri = new URI("/conta/soma");
+		String json = "{}";
+
+		mockMvc.perform(MockMvcRequestBuilders.get(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+}
